4cb5f20aa7d2114168346e7d83f9078bbf06fb00
Masaaki Horikawa <Masaaki Horikawa@192.168.100.122>
2014-07-03 18:03:22 +0900

    登録レイアウトファイル作成
    Signed-off-by: Masaaki Horikawa <Masaaki Horikawa@172.17.1.45>

--- Sawara/src/jp/ac/bemax/sawara/TitleActivity.java
+++ Sawara/src/jp/ac/bemax/sawara/TitleActivity.java
@@ -3,13 +3,29 @@ package jp.ac.bemax.sawara;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class TitleActivity extends Activity {
+/**
+ * タイトル画面のアクティビティ
+ * @author Masaaki Horikawa
+ * 2014/07/02
+ */
+public class TitleActivity extends Activity implements OnClickListener{
 	private TextView titleView;
 	private Button searchButton, registerButton;
+	private float density;
 	
+	/* (非 Javadoc)
+	 * コンストラクタ
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,7 +38,7 @@ public class TitleActivity extends Activity {
 		searchButton = (Button)findViewById(R.id.search_button);
 		registerButton = (Button)findViewById(R.id.register_button);
 		
-		/* フォントを登録 */
+		/* フォントを読み込む */
 		Typeface yasashisa_B = Typeface.createFromAsset(getAssets(),"yasashisa_bold.ttf");
 		Typeface yasashisa = Typeface.createFromAsset(getAssets(),"yasashisa.ttf");
 		
@@ -31,5 +47,69 @@ public class TitleActivity extends Activity {
 		searchButton.setTypeface(yasashisa);
 		registerButton.setTypeface(yasashisa);
 		
+		/* リスナの登録 */
+		searchButton.setOnClickListener(this);
+		registerButton.setOnClickListener(this);
+		
+		/* ディスプレイ情報を取得 */
+		WindowManager wm = getWindowManager();
+		Display display = wm.getDefaultDisplay();
+		DisplayMetrics dm = new DisplayMetrics();
+		display.getMetrics(dm);
+		density = dm.density;
+		Log.d("DisplayDensity",""+dm.density);
 	}
+
+	/* (非 Javadoc)
+	 * ボタンがクリックされたときに呼び出されるメソッド
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		
+		/* クリックされたボタンによって分岐 */
+		switch(v.getId()){
+		case R.id.search_button:
+			// さがすボタンがクリックされたとき
+			
+			/* ログ出力 */
+			Log.d("clicked Button","SearchButton");
+			
+			break;
+		case R.id.register_button:
+			// とうろくするボタンがクリックされたとき
+			
+			/* ログ出力 */
+			Log.d("clicked Button","RegistarButton");
+		
+			break;
+		}
+		
+	}
+
+	/* (非 Javadoc)
+	 * 画面が表示されたときに呼び出されるメソッド
+	 * @see android.app.Activity#onWindowFocusChanged(boolean)
+	 */
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		
+		int sb_height = searchButton.getHeight();
+		int sb_width = searchButton.getWidth();
+		int tv_width = titleView.getWidth();
+		
+		float max_height = sb_height/3;
+		float max_width = sb_width * 0.5f / searchButton.getText().length();
+		float bt_text_size = (max_height < max_width) ? max_height : max_width;
+		searchButton.setTextSize(bt_text_size/density);
+		
+		max_width = sb_width * 0.5f / registerButton.getText().length();
+		bt_text_size = (max_height < max_width) ? max_height : max_width;
+		registerButton.setTextSize(bt_text_size/density);
+		
+		titleView.setTextSize(tv_width/(titleView.getText().length()+1)/density);
+	}
+	
+	
 }
