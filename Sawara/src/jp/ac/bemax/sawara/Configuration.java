82153e0bb85677a7f04664ba36098510d4f8874f
Masaaki Horikawa <Masaaki Horikawa@192.168.100.154>
2014-10-30 14:58:01 +0900

    KJ-10 Configrationクラス作成。Configファイルの操作を行うクラス。

---
+++ Sawara/src/jp/ac/bemax/sawara/Configuration.java
@@ -0,0 +1,119 @@
+package jp.ac.bemax.sawara;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import android.util.Log;
+
+public class Configuration {
+	static File confFile = null;
+	
+	/**
+	 * configファイルから値を取り出す。
+	 * @param valName 変数名
+	 * @return 変数の値
+	 */
+	static String getConfig(String valName){
+		String result = null;
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new FileReader(confFile));
+			String readData = null;
+			
+			breakPoint:
+			while((readData = br.readLine()) != null){
+				String[] str = readData.split(";");
+				for(String s: str){
+					String[] ss = s.split("=");
+					if(ss[0].equals(valName)){
+						result = ss[1];
+						break breakPoint;
+					}
+				}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				br.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		
+		Log.d("configVal", result);
+		return result;
+	}
+	
+	/**
+	 * configファイルに値を設定する
+	 * @param name 変数名
+	 * @param val 値
+	 */
+	static void setConfig(String name, String val){
+		StringBuffer buffer = new StringBuffer("");
+		FileReader fr = null;
+		BufferedReader br = null;
+		FileWriter fw = null;
+		
+		try{
+			br = new BufferedReader(new FileReader(confFile));
+			String readData = null;
+			while((readData = br.readLine()) != null){
+				String[] str = readData.split(";");
+				for(String s: str){
+					String writeStr = readData;
+					String[] ss = readData.split("=");
+					if(ss[0].equals(name)){
+						writeStr = name + "=" + val + ";";
+					}
+					buffer.append(writeStr);
+				}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try{
+			fw = new FileWriter(confFile);
+			fw.write(buffer.toString());
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try {
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * configファイルを作成する
+	 */
+	static void makeConfFile(){
+		BufferedWriter bw = null;
+		try{
+			bw = new BufferedWriter(new FileWriter(confFile));
+			bw.write("theme=DefaultTheme;");
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				bw.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- Sawara/src/jp/ac/bemax/sawara/HomeActivity.java
+++ Sawara/src/jp/ac/bemax/sawara/HomeActivity.java
@@ -1,12 +1,6 @@
 package jp.ac.bemax.sawara;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,56 +60,14 @@ public class HomeActivity extends Activity implements OnClickListener, OnMenuIte
 		/***********
 		 * 設定ファイルの処理
 		 ***********/
-		File dir = getFilesDir();
-		File f = new File(dir, "config.ini");
-		FileWriter fw = null;
-		if(!f.exists()){
-			try {
-				fw = new FileWriter(f);
-				// システムの初期設定を書き込む
-				fw.write("theme = GorstTheme");			
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}finally{
-				try {
-					fw.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+		Configuration.confFile = new File(getFilesDir(), "sawara.conf");
+		
+		if(!Configuration.confFile.exists()){
+			Configuration.makeConfFile();
 		}
-		FileReader fr = null;
-		BufferedReader br = null;
-		try{
-			// 初期設定ファイルから読み込む
-			fr = new FileReader(f);
-			br = new BufferedReader(fr);
-			String s;
-			while((s = br.readLine()) != null){
-				String[] ss = s.split("=");
-				
-				// テーマの初期設定を処理
-				if(ss[0].trim().equals("theme")){
-					// 文字列からリソースIDを取得
-					int resid = getResources().getIdentifier(ss[1].trim(), "style", getPackageName());
-					// テーマをセットする
-					setTheme(resid);
-				}
-			}
-		}catch(FileNotFoundException e){
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			try{
-				br.close();
-				fr.close();
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-		}
+		String themeVal = Configuration.getConfig("theme");
+		int resid = getResources().getIdentifier(themeVal, "style", getPackageName());
+		setTheme(resid);
 		
 		/***********
 		 * データベースアクセス
@@ -226,12 +178,15 @@ public class HomeActivity extends Activity implements OnClickListener, OnMenuIte
 		switch(item.getItemId()){
 		case 0:
 			this.setTheme(R.style.GorstTheme);
+			Configuration.setConfig("theme", "GorstTheme");
 			break;
 		case 1:
-			this.setTheme(R.style.HeratTheme);
+			this.setTheme(R.style.HeartTheme);
+			Configuration.setConfig("theme", "HeartTheme");
 			break;
 		case 2:
 			this.setTheme(R.style.StarTheme);
+			Configuration.setConfig("theme", "StarTheme");
 			break;
 		}
 		mHandler.sendEmptyMessage(THEME_CHANGE);
